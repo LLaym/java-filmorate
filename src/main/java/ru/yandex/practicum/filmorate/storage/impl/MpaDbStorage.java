d7fbd5fe4f1350e06726183ed9f19ea8b57b2f27
LLaym <vornysun@yandex.ru>
2023-04-24 19:33:10 +0300

    feat: write methods on MpaDbStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/impl/MpaDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/MpaDbStorage.java
@@ -1,22 +1,38 @@
 package ru.yandex.practicum.filmorate.storage.impl;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.storage.MpaStorage;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collection;
 
 @Repository
 public class MpaDbStorage implements MpaStorage {
+    private final JdbcTemplate jdbcTemplate;
+
+    public MpaDbStorage(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public Collection<Mpa> getAllMpas() {
-        // TODO реализовать
-        return null;
+        String sql = "SELECT * FROM mpas ORDER BY id";
+
+        return jdbcTemplate.query(sql, ((rs, rowNum) -> makeMpa(rs)));
     }
 
     @Override
     public Mpa getMpaById(Integer id) {
-        // TODO реализовать
-        return null;
+        String sql = "SELECT * FROM mpas WHERE id = ?";
+
+        return jdbcTemplate.query(sql, ((rs, rowNum) -> makeMpa(rs)), id)
+                .stream().findFirst().orElse(null);
+    }
+
+    private Mpa makeMpa(ResultSet rs) throws SQLException {
+        return new Mpa(rs.getInt("id"), rs.getString("name"));
     }
 }
