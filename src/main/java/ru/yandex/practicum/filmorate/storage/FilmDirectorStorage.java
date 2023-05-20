72dd2d08948cd046537ff5db599c32a5989eb501
LLaym <vornysun@yandex.ru>
2023-05-20 14:56:07 +0300

    feature: добавить сущн. FilmDirector, так же FilmDirectorStorage, FilmDirectorDbStorage

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmDirectorStorage.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.FilmDirector;
+import ru.yandex.practicum.filmorate.model.FilmGenre;
+
+import java.util.List;
+
+public interface FilmDirectorStorage {
+    void save(int filmId, int directorId);
+
+    boolean deleteAllByFilmId(int filmId);
+
+    List<FilmDirector> getAllByFilmId(int filmId);
+}
